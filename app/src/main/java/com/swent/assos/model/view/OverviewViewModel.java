b521f912c78e0ee70dfbf5f996379fea402c32a0
Réza <reza.machraoui@gmail.com>
2024-03-26 14:31:41 +0100

    Fix OverviewViewModel

--- app/src/main/java/com/swent/assos/model/view/OverviewViewModel.java
+++
@@ -1,4 +0,0 @@
-package com.swent.assos.model.view;
-
-public class OverviewViewModel {
-}
--- app/src/main/java/com/swent/assos/ui/screens/Overview.kt
+++ app/src/main/java/com/swent/assos/ui/screens/Overview.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.swent.assos.model.view.OverviewViewModel
 
 @Composable
