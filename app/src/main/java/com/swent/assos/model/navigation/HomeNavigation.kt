54cf988060b76e464ebb37bada417159fc89a5d9
Quentin Gross <quentin.gross@epfl.ch>
2024-03-26 14:21:42 +0100

    HomeNavigation

---
+++ app/src/main/java/com/swent/assos/model/navigation/HomeNavigation.kt
@@ -0,0 +1,105 @@
+package com.swent.assos.model.navigation
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.swent.assos.R
+import com.swent.assos.ui.screens.Calendar
+import com.swent.assos.ui.screens.Explorer
+import com.swent.assos.ui.screens.News
+import com.swent.assos.ui.screens.Profil
+import com.swent.assos.ui.screens.QrCode
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HomeNavigation() {
+    val coroutineScope = rememberCoroutineScope()
+    val pagerState = rememberPagerState(initialPage = 0, initialPageOffsetFraction = 0f) { 2 }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        HorizontalPager(
+            state = pagerState,
+            userScrollEnabled = false,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),
+        ) { page ->
+            when (page) {
+                0 -> News()
+                1 -> Explorer()
+                2 -> Calendar()
+                3 -> QrCode()
+                4 -> Profil()
+            }
+        }
+
+        NavigationBar(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface),
+            containerColor = Color.Transparent,
+        ) {
+            repeat(2) { index ->
+                NavigationBarItem(
+                    modifier = Modifier.width(30.dp),
+                    icon = {
+                        when (index) {
+                            0 ->
+                                Icon(
+                                    painterResource(id = R.drawable.menu),
+                                    contentDescription = "Overview",
+                                    tint = MaterialTheme.colorScheme.onSurface)
+                            1 ->
+                                Icon(
+                                    painterResource(id = R.drawable.menu),
+                                    contentDescription = "Map",
+                                    tint = MaterialTheme.colorScheme.onSurface)
+                            2 ->
+                                Icon(
+                                    painterResource(id = R.drawable.menu),
+                                    contentDescription = "Map",
+                                    tint = MaterialTheme.colorScheme.onSurface)
+                            3 ->
+                                Icon(
+                                    painterResource(id = R.drawable.menu),
+                                    contentDescription = "Map",
+                                    tint = MaterialTheme.colorScheme.onSurface)
+                            4 ->
+                                Icon(
+                                    painterResource(id = R.drawable.menu),
+                                    contentDescription = "Map",
+                                    tint = MaterialTheme.colorScheme.onSurface)
+                        }
+                    },
+                    selected = pagerState.currentPage == index,
+                    onClick = { coroutineScope.launch { pagerState.animateScrollToPage(index) } },
+                    colors =
+                    NavigationBarItemDefaults.colors(
+                        indicatorColor = MaterialTheme.colorScheme.secondaryContainer,
+                    ),
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/swent/assos/model/navigation/NavigationBar.kt
+++
@@ -1,2 +0,0 @@
-package com.swent.assos.model.navigation
-
