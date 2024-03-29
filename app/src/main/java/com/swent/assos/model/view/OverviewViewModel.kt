34ebce31a82d0253be675b87eb4b4c7b660d7492
Réza <reza.machraoui@gmail.com>
2024-03-29 18:37:52 +0100

    search bar version 1.0

--- app/src/main/java/com/swent/assos/model/view/OverviewViewModel.kt
+++ app/src/main/java/com/swent/assos/model/view/OverviewViewModel.kt
@@ -6,6 +6,7 @@ import com.swent.assos.model.data.Association
 import com.swent.assos.model.service.AuthService
 import com.swent.assos.model.service.DbService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -13,15 +14,35 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 @HiltViewModel
-class OverviewViewModel
-@Inject
-constructor(private val dbService: DbService, private val authService: AuthService) : ViewModel() {
+class OverviewViewModel @Inject constructor(
+  private val dbService: DbService,
+  private val authService: AuthService
+): ViewModel() {
   private val _allAssociations = MutableStateFlow(emptyList<Association>())
   val allAssociations = _allAssociations.asStateFlow()
 
+  private val _filteredAssociations = MutableStateFlow(emptyList<Association>())
+  val filteredAssociations = _filteredAssociations.asStateFlow()
+
   init {
-    viewModelScope.launch(Dispatchers.IO) {
-      dbService.getAllAssociations().let { _allAssociations.value = it }
+      viewModelScope.launch(Dispatchers.IO) {
+        dbService.getAllAssociations().let {
+          _allAssociations.value = it
+          _filteredAssociations.value = it
+        }
+      }
+  }
+
+  fun filterOnSearch(query: String) {
+    _filteredAssociations.value = _allAssociations.value.filter {
+      it.acronym.contains(query, ignoreCase = true) || it.fullname.contains(query, ignoreCase = true)
     }
   }
+
+
+
 }
+
+
+
+
--- app/src/main/java/com/swent/assos/ui/screens/Overview.kt
+++ app/src/main/java/com/swent/assos/ui/screens/Overview.kt
@@ -1,21 +1,35 @@
 package com.swent.assos.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DockedSearchBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.swent.assos.model.data.Association
@@ -23,9 +37,9 @@ import com.swent.assos.model.view.OverviewViewModel
 
 @Composable
 fun Overview(overviewViewModel: OverviewViewModel) {
-  val associations = overviewViewModel.allAssociations.collectAsState()
+  val associations = overviewViewModel.filteredAssociations.collectAsState()
 
-  Scaffold { paddingValues ->
+  Scaffold(topBar = { TopResearchBar(overviewViewModel = overviewViewModel) }) { paddingValues ->
     LazyColumn(modifier = Modifier.padding(paddingValues), userScrollEnabled = true) {
       if (associations.value.isEmpty()) {
         item { Text(text = "No results were found", textAlign = TextAlign.Center) }
@@ -53,4 +67,42 @@ fun ListItemFrom(asso: Association) {
       },
       colors = ListItemDefaults.colors(containerColor = Color.Transparent),
       modifier = Modifier.padding(start = 26.dp, end = 26.dp))
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopResearchBar(overviewViewModel: OverviewViewModel) {
+
+  var query by remember { mutableStateOf("") }
+  var isSearching by remember { mutableStateOf(false) }
+  val focusManager = LocalFocusManager.current
+
+  Column(modifier = Modifier.fillMaxWidth().padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    DockedSearchBar(
+      leadingIcon = { Image(imageVector = Icons.Default.Home, contentDescription = null) },
+      trailingIcon = { 
+        if (isSearching) { 
+          Image(imageVector = Icons.Default.Close,
+                contentDescription = null,
+                modifier = 
+                  Modifier.clickable {
+                            query = ""
+                            overviewViewModel.filterOnSearch("")
+                            focusManager.clearFocus()
+                            isSearching = false
+                          })
+        } else {
+          Image(imageVector = Icons.Default.Search, contentDescription = null)
+        }
+      },
+      placeholder = { Text(text = "Search an Association") },
+      query = query,
+      onQueryChange = {
+        query = it
+        overviewViewModel.filterOnSearch(it)
+                      },
+      onSearch = {},
+      active = false,
+      onActiveChange = { isSearching = it}) {}
+  }
 }
