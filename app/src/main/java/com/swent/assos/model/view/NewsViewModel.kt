9d82f49d9e9b12da089c66ebbd81b235243ef9c6
Jules <jules.herrscher@epfl.ch>
2024-04-12 01:01:11 +0200

    Fixed the injection of the dispatcher

--- app/src/main/java/com/swent/assos/model/view/NewsViewModel.kt
+++ app/src/main/java/com/swent/assos/model/view/NewsViewModel.kt
@@ -4,11 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.swent.assos.model.data.Association
 import com.swent.assos.model.data.News
+import com.swent.assos.model.di.IoDispatcher
 import com.swent.assos.model.service.DbService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -18,18 +18,16 @@ class NewsViewModel
 @Inject
 constructor(
     private val dbService: DbService,
-    private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
   private val _allNews = MutableStateFlow(emptyList<News>())
   val allNews = _allNews.asStateFlow()
 
   init {
-    viewModelScope.launch(defaultDispatcher) { dbService.getAllNews().let { _allNews.value = it } }
+    viewModelScope.launch(ioDispatcher) { dbService.getAllNews().let { _allNews.value = it } }
   }
 
   fun getNewsAssociation(associationId: String, callback: (Association) -> Unit) {
-    viewModelScope.launch(defaultDispatcher) {
-      callback(dbService.getAssociationById(associationId))
-    }
+    viewModelScope.launch(ioDispatcher) { callback(dbService.getAssociationById(associationId)) }
   }
 }
