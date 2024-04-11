b8ffc26826c7bfa6dcf3e872bae55d8caa5eeb1a
Maximilien <maximilien.gridel@gmail.com>
2024-04-11 18:18:38 +0200

    - real colors added - logo added - installation of logos

--- app/src/main/java/com/swent/assos/ui/theme/Theme.kt
+++ app/src/main/java/com/swent/assos/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -20,17 +21,15 @@ private val DarkColorScheme =
 
 private val LightColorScheme =
     lightColorScheme(
-        primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40
+        primary = RedEpfl, secondary = GroseilleEpfl, tertiary = White,
 
-        /* Other default colors to override
-        background = Color(0xFFFFFBFE),
-        surface = Color(0xFFFFFBFE),
-        onPrimary = Color.White,
-        onSecondary = Color.White,
-        onTertiary = Color.White,
-        onBackground = Color(0xFF1C1B1F),
-        onSurface = Color(0xFF1C1B1F),
-        */
+        background = White,
+        surface = White,
+        onPrimary = White ,
+        onSecondary = White,
+        onTertiary = Black,
+        onBackground = Black,
+        onSurface = Gray,
         )
 
 @Composable
