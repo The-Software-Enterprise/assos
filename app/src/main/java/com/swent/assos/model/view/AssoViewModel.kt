a040e822e6869b5d234281a5ae6da3bc46f3b44a
alicepotter <alice.potter@epfl.ch>
2024-04-17 17:03:54 +0200

    Backend follow button

---
+++ app/src/main/java/com/swent/assos/model/view/AssoViewModel.kt
@@ -0,0 +1,28 @@
+package com.swent.assos.model.view
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.swent.assos.model.data.Event
+import com.swent.assos.model.di.IoDispatcher
+import com.swent.assos.model.service.DbService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class AssoViewModel
+@Inject
+constructor(
+    private val dbService: DbService,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) : ViewModel() {
+
+    fun followAssociation(associationId: String) {
+        viewModelScope.launch(ioDispatcher) {
+            dbService.followAssociation(associationId, {}, {})
+        }
+    }
+}
