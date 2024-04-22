5007a2568839103e1dcb4fc3c30d859a09856a35
Quentin Gross <quentin.gross@epfl.ch>
2024-04-22 11:59:40 +0200

    Tests fix

--- app/src/androidTest/java/com/swent/assos/SuperTest.kt
+++ app/src/androidTest/java/com/swent/assos/SuperTest.kt
@@ -1,21 +1,16 @@
 package com.swent.assos
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import com.swent.assos.model.navigation.NavigationActions
 import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
 import org.junit.Before
 import org.junit.Rule
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-@HiltAndroidTest
 open class SuperTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
 
   @get:Rule(order = 1) var hiltRule = HiltAndroidRule(this)
