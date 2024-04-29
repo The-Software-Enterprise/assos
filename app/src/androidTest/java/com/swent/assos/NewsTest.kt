a6605160c09b863ab5926bd900675caa8f6f3114
Jules <jules.herrscher@epfl.ch>
2024-04-29 17:54:07 +0200

    fixed testing of news

--- app/src/androidTest/java/com/swent/assos/NewsTest.kt
+++ app/src/androidTest/java/com/swent/assos/NewsTest.kt
@@ -6,16 +6,23 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import com.swent.assos.model.navigation.Destinations
 import com.swent.assos.model.navigation.NavigationActions
 import com.swent.assos.screens.CreateNewsScreen
+import com.swent.assos.screens.LoginScreen
 import com.swent.assos.screens.NewsScreen
+import com.swent.assos.screens.SignupScreen
+import com.swent.assos.ui.login.LoginScreen
+import com.swent.assos.ui.login.SignUpScreen
 import com.swent.assos.ui.screens.News
 import com.swent.assos.ui.screens.manageAsso.CreateNews
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.mockk.confirmVerified
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
+import io.mockk.verify
 import kotlin.random.Random
 import org.junit.Before
 import org.junit.Rule
@@ -51,6 +58,55 @@ class NewsTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSuppor
 
   @Test
   fun createNewsAndVerifyCreation() {
+
+    composeTestRule.activity.setContent { SignUpScreen(navigationActions = mockNavActions) }
+    run {
+      ComposeScreen.onComposeScreen<SignupScreen>(composeTestRule) {
+        step("Signup") {
+          emailField {
+            assertIsDisplayed()
+            performTextInput("test@epfl.ch")
+          }
+          passwordField {
+            assertIsDisplayed()
+            performTextInput("test1234")
+          }
+          confirmPasswordField {
+            assertIsDisplayed()
+            performTextInput("test1234")
+          }
+          signUpButton {
+            assertIsDisplayed()
+            performClick()
+          }
+        }
+        Thread.sleep(1000)
+        verify { mockNavActions.navigateTo(Destinations.HOME) }
+        confirmVerified(mockNavActions)
+      }
+    }
+    composeTestRule.activity.setContent { LoginScreen(navigationActions = mockNavActions) }
+    run {
+      ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
+        step("Signin with good credentials") {
+          emailField {
+            assertIsDisplayed()
+            performTextInput("test@epfl.ch")
+          }
+          passwordField {
+            assertIsDisplayed()
+            performTextInput("test1234")
+          }
+          loginButton {
+            assertIsDisplayed()
+            performClick()
+          }
+        }
+      }
+      Thread.sleep(1000)
+      verify { mockNavActions.navigateTo(Destinations.HOME.route) }
+      confirmVerified(mockNavActions)
+    }
 
     composeTestRule.activity.setContent {
       CreateNews(navigationActions = mockNavActions, assoId = "jMWo6NgngIS2hCq054TF")
