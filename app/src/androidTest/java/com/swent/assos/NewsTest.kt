135e4bb2df686c9e4e83ff3f41312047eb3eea0b
Jules <jules.herrscher@epfl.ch>
2024-04-29 19:13:43 +0200

    tried to fix the tests by authenticating at setup

--- app/src/androidTest/java/com/swent/assos/NewsTest.kt
+++ app/src/androidTest/java/com/swent/assos/NewsTest.kt
@@ -3,26 +3,20 @@ package com.swent.assos
 import androidx.activity.compose.setContent
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.firebase.auth.FirebaseAuth
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
-import com.swent.assos.model.navigation.Destinations
 import com.swent.assos.model.navigation.NavigationActions
 import com.swent.assos.screens.CreateNewsScreen
-import com.swent.assos.screens.LoginScreen
 import com.swent.assos.screens.NewsScreen
-import com.swent.assos.screens.SignupScreen
-import com.swent.assos.ui.login.LoginScreen
-import com.swent.assos.ui.login.SignUpScreen
 import com.swent.assos.ui.screens.News
 import com.swent.assos.ui.screens.manageAsso.CreateNews
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.github.kakaocup.compose.node.element.ComposeScreen
-import io.mockk.confirmVerified
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
-import io.mockk.verify
 import kotlin.random.Random
 import org.junit.Before
 import org.junit.Rule
@@ -43,13 +37,15 @@ class NewsTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSuppor
   // Relaxed mocks methods have a default implementation returning values
   @RelaxedMockK lateinit var mockNavActions: NavigationActions
 
-  var associationAcronym = "180°C"
-
-  val newsHeader = "Test description -1934310868"
-
   @Before
   fun setup() {
     hiltRule.inject()
+    FirebaseAuth.AuthStateListener { auth ->
+      auth.createUserWithEmailAndPassword("test@epfl.ch", "test12")
+    }
+    FirebaseAuth.AuthStateListener { auth ->
+      auth.signInWithEmailAndPassword("test@epfl.ch", "test12")
+    }
   }
 
   private val randomInt = Random.nextInt()
@@ -58,85 +54,38 @@ class NewsTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSuppor
 
   @Test
   fun createNewsAndVerifyCreation() {
+    composeTestRule.activity.setContent {
+      CreateNews(navigationActions = mockNavActions, assoId = "jMWo6NgngIS2hCq054TF")
+    }
+    run {
+      ComposeScreen.onComposeScreen<CreateNewsScreen>(composeTestRule) {
+        step("Create a news") {
+          form { assertIsDisplayed() }
+          inputTitle {
+            assertIsDisplayed()
+            performClick()
+            performTextInput(newsTitle)
+          }
+          inputDescription {
+            assertIsDisplayed()
+            performClick()
+            performTextInput(newsDescription)
+          }
+          buttonSave { performClick() }
+        }
+      }
+    }
+
+    composeTestRule.activity.setContent { News() }
 
     run {
-      composeTestRule.activity.setContent { SignUpScreen(navigationActions = mockNavActions) }
-      ComposeScreen.onComposeScreen<SignupScreen>(composeTestRule) {
-        step("Signup") {
-          emailField {
-            assertIsDisplayed()
-            performTextInput("test@epfl.ch")
-          }
-          passwordField {
-            assertIsDisplayed()
-            performTextInput("test1234")
-          }
-          confirmPasswordField {
-            assertIsDisplayed()
-            performTextInput("test1234")
-          }
-          signUpButton {
-            assertIsDisplayed()
-            performClick()
-          }
-        }
-        Thread.sleep(1000)
-        verify { mockNavActions.navigateTo(Destinations.HOME) }
-        confirmVerified(mockNavActions)
-      }
-      composeTestRule.activity.setContent { LoginScreen(navigationActions = mockNavActions) }
-      ComposeScreen.onComposeScreen<LoginScreen>(composeTestRule) {
-        step("Signin with good credentials") {
-          emailField {
-            assertIsDisplayed()
-            performTextInput("test@epfl.ch")
-          }
-          passwordField {
-            assertIsDisplayed()
-            performTextInput("test1234")
-          }
-          loginButton {
-            assertIsDisplayed()
-            performClick()
-          }
-        }
-        Thread.sleep(1000)
-        verify { mockNavActions.navigateTo(Destinations.HOME.route) }
-        confirmVerified(mockNavActions)
-        composeTestRule.activity.setContent {
-          CreateNews(navigationActions = mockNavActions, assoId = "jMWo6NgngIS2hCq054TF")
-        }
-        ComposeScreen.onComposeScreen<CreateNewsScreen>(composeTestRule) {
-          step("Create a news") {
-            form { assertIsDisplayed() }
-            inputTitle {
-              assertIsDisplayed()
-              performClick()
-              performTextInput(newsTitle)
-            }
-            inputDescription {
-              assertIsDisplayed()
-              performClick()
-              performTextInput(newsDescription)
-            }
-            buttonSave { performClick() }
-          }
-        }
-      }
-
-      composeTestRule.activity.setContent { News() }
-
-      run {
-        ComposeScreen.onComposeScreen<NewsScreen>(composeTestRule) {
-          step("Check if the news is displayed") {
-            newsList { assertIsDisplayed() }
-            newsListItems { assertIsDisplayed() }
-            itemsTitle { assertTextContains(newsTitle) }
-            itemsDescription { assertTextContains(newsDescription) }
-            itemsAssociation {
-              assertTextContains("Association to promote cooking amongst students")
-            }
-          }
+      ComposeScreen.onComposeScreen<NewsScreen>(composeTestRule) {
+        step("Check if the news is displayed") {
+          newsList { assertIsDisplayed() }
+          newsListItems { assertIsDisplayed() }
+          itemsTitle { assertTextContains(newsTitle) }
+          itemsDescription { assertTextContains(newsDescription) }
+          itemsAssociation { assertTextContains("Association to promote cooking amongst students") }
         }
       }
     }
