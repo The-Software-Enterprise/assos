cfe99fc38effb9f71962a3dd3e5a52b413564cb7
Quentin Gross <quentin.gross@epfl.ch>
2024-05-02 16:56:09 +0200

    deleted useless component

--- app/src/main/java/com/swent/assos/ui/screens/manageAsso/CreateNews.kt
+++ app/src/main/java/com/swent/assos/ui/screens/manageAsso/CreateNews.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -28,14 +27,11 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.DeleteForever
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
@@ -43,8 +39,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -257,46 +251,4 @@ fun CreateNews(
           }
         }
       }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AddImage(onDismissRequest: () -> Unit, onConfirmation: (String) -> Unit) {
-
-  var url by remember { mutableStateOf("") }
-
-  AlertDialog(onDismissRequest = { onDismissRequest() }) {
-    Surface(
-        modifier = Modifier.width(400.dp).height(250.dp),
-        color = MaterialTheme.colorScheme.background,
-        shape = RoundedCornerShape(size = 8.dp)) {
-          Column(
-              modifier = Modifier.fillMaxSize().testTag("AddImageDialog"),
-              horizontalAlignment = Alignment.CenterHorizontally) {
-                Spacer(modifier = Modifier.height(16.dp))
-                Text("Add Image", fontSize = 30.sp, fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(16.dp))
-
-                OutlinedTextField(
-                    modifier = Modifier.testTag("InputImage"),
-                    value = url,
-                    onValueChange = { url = it },
-                    label = { Text("Image URL") })
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Row {
-                  Button(
-                      onClick = { onDismissRequest() },
-                      modifier = Modifier.testTag("CancelImage")) {
-                        Text("Cancel")
-                      }
-                  Spacer(modifier = Modifier.width(16.dp))
-                  Button(
-                      onClick = { onConfirmation(url) }, modifier = Modifier.testTag("SaveImage")) {
-                        Text("Confirm")
-                      }
-                }
-              }
-        }
-  }
 }
