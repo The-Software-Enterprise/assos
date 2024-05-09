f2daad143466918f2925800b08de0ad5c3679ec2
Réza <reza.machraoui@gmail.com>
2024-05-10 01:28:49 +0200

    wait for calendar to be displayed

--- app/src/androidTest/java/com/swent/assos/CalendarTest.kt
+++ app/src/androidTest/java/com/swent/assos/CalendarTest.kt
@@ -6,10 +6,12 @@ import com.swent.assos.screens.CalendarScreen
 import com.swent.assos.ui.screens.calendar.Calendar
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.github.kakaocup.compose.node.element.ComposeScreen
+import okhttp3.internal.wait
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.lang.Thread.sleep
 
 private val titleDateFormatter = DateTimeFormatter.ofPattern("dd LLL uuuu")
 
@@ -33,6 +35,7 @@ class CalendarTest : SuperTest() {
                 LocalDate.now().format(titleDateFormatter), ignoreCase = true, substring = true)
           }
           titleItemSelected {
+            sleep(3000)
             assertIsDisplayed()
             assertTextContains(
                 LocalDate.now().dayOfMonth.toString(), ignoreCase = true, substring = true)
