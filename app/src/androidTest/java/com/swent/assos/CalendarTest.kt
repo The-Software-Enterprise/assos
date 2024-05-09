da676d1467266c16f5ec950c56347056c4975de1
Réza <reza.machraoui@gmail.com>
2024-05-10 01:34:37 +0200

    format

--- app/src/androidTest/java/com/swent/assos/CalendarTest.kt
+++ app/src/androidTest/java/com/swent/assos/CalendarTest.kt
@@ -6,12 +6,11 @@ import com.swent.assos.screens.CalendarScreen
 import com.swent.assos.ui.screens.calendar.Calendar
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.github.kakaocup.compose.node.element.ComposeScreen
-import okhttp3.internal.wait
+import java.lang.Thread.sleep
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Thread.sleep
 
 private val titleDateFormatter = DateTimeFormatter.ofPattern("dd LLL uuuu")
 
