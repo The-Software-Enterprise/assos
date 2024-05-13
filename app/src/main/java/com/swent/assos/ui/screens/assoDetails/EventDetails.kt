6e0fe84c5bf6ec8e786adfa5891885f72a3117a4
alicepotter <alice.potter@epfl.ch>
2024-05-13 11:54:53 +0200

    Changed and if to a when

--- app/src/main/java/com/swent/assos/ui/screens/assoDetails/EventDetails.kt
+++ app/src/main/java/com/swent/assos/ui/screens/assoDetails/EventDetails.kt
@@ -5,12 +5,9 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -18,7 +15,6 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
@@ -34,18 +30,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
 import com.swent.assos.R
@@ -95,41 +86,42 @@ fun EventDetails(eventId: String, navigationActions: NavigationActions, assoId: 
       topBar = { TopNewsBar(asso, navigationActions, event) },
       floatingActionButton = {
         if (eventId != "") {
-          if (isMember(myAssociations = myAssociations, cuurentAsso = asso.id)) {
-            CreateTicketButton(navigationActions)
-          } else {
-            JoinUsButton(onClick = { confirming = true }, text = "Become Staff")
+          when (isMember(myAssociations = myAssociations, currentAsso = asso.id)) {
+            true ->
+                JoinUsButton(
+                    onClick = { navigationActions.navigateTo(Destinations.CREATE_TICKET.route) },
+                    text = "Create ticket")
+            false -> JoinUsButton(onClick = { confirming = true }, text = "Become Staff")
           }
         }
       },
-      floatingActionButtonPosition = FabPosition.Center,
-  ) { paddingValues ->
-    LazyColumn(
-        modifier = Modifier.testTag("EventDetailsList").padding(paddingValues),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-      item {
-        Image(
-            painter =
-                if (event.image != Uri.EMPTY) {
-                  rememberAsyncImagePainter(event.image)
-                } else {
-                  painterResource(id = R.drawable.ic_launcher_foreground)
-                },
-            contentDescription = null,
-            modifier =
-                Modifier.padding(10.dp)
-                    .height(200.dp)
-                    .clip(shape = RoundedCornerShape(20.dp))
-                    .background(MaterialTheme.colorScheme.background)
-                    .fillMaxWidth(),
-            alignment = Alignment.Center,
-            contentScale = ContentScale.Crop)
-      }
+      floatingActionButtonPosition = FabPosition.Center) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.testTag("EventDetailsList").padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+          item {
+            Image(
+                painter =
+                    if (event.image != Uri.EMPTY) {
+                      rememberAsyncImagePainter(event.image)
+                    } else {
+                      painterResource(id = R.drawable.ic_launcher_foreground)
+                    },
+                contentDescription = null,
+                modifier =
+                    Modifier.padding(10.dp)
+                        .height(200.dp)
+                        .clip(shape = RoundedCornerShape(20.dp))
+                        .background(MaterialTheme.colorScheme.background)
+                        .fillMaxWidth(),
+                alignment = Alignment.Center,
+                contentScale = ContentScale.Crop)
+          }
 
-      item { Text(text = event.description, modifier = Modifier.padding(10.dp)) }
-    }
-  }
+          item { Text(text = event.description, modifier = Modifier.padding(10.dp)) }
+        }
+      }
 }
 
 @Composable
@@ -177,39 +169,11 @@ fun ConfirmButton(onConfirm: () -> Unit, text: String = "Yes") {
 }
 
 @Composable
-fun isMember(myAssociations: List<Association>, cuurentAsso: String): Boolean {
-  var isMember = false
+fun isMember(myAssociations: List<Association>, currentAsso: String): Boolean {
   for (asso in myAssociations) {
-    if (asso.id == cuurentAsso) {
-      isMember = true
+    if (asso.id == currentAsso) {
+      return true
     }
   }
-  return isMember
-}
-
-@Composable
-fun CreateTicketButton(navigationActions: NavigationActions) {
-  Row(
-      modifier = Modifier.fillMaxWidth(),
-      horizontalArrangement = Arrangement.Center,
-  ) {
-    FloatingActionButton(
-        onClick = { navigationActions.navigateTo(Destinations.CREATE_TICKET.route) },
-        modifier =
-            Modifier.shadow(8.dp, shape = RoundedCornerShape(25), clip = false)
-                .background(color = Color(0xFF5465FF), shape = RoundedCornerShape(size = 16.dp))
-                .then(Modifier.widthIn(min = 92.dp))
-                .height(42.dp)
-                .testTag("CreateTicketButton"),
-        containerColor = Color(0xFF5465FF),
-    ) {
-      Text(
-          text = "Create ticket",
-          fontSize = 16.sp,
-          fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-          fontWeight = FontWeight.SemiBold,
-          color = MaterialTheme.colorScheme.onPrimary,
-          style = MaterialTheme.typography.bodyMedium)
-    }
-  }
+  return false
 }
