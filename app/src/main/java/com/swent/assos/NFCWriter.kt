46c529bfeffbff40cde7385adc0a7635e84a0e92
Réza <reza.machraoui@gmail.com>
2024-05-15 15:11:30 +0200

    adapt activites to be launched on phone and write meaningful data

--- app/src/main/java/com/swent/assos/NFCWriter.kt
+++ app/src/main/java/com/swent/assos/NFCWriter.kt
@@ -23,176 +23,178 @@ import dagger.hilt.android.AndroidEntryPoint
 import java.io.IOException
 import kotlinx.coroutines.flow.MutableStateFlow
 
-
 @AndroidEntryPoint
 class NFCWriter : ComponentActivity() {
 
-    private var nfcAdapter: NfcAdapter? = null
-    val _msgList = MutableStateFlow(emptyList<String>())
-    lateinit var eventId: String
+  private var nfcAdapter: NfcAdapter? = null
+  private val ticketList = MutableStateFlow(emptyList<String>())
+  lateinit var eventID: String
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AssosTheme {
-                NFCWriting(msgListState = _msgList)
-            }
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    // start l'activity comme ceci (cf. ChatGPT)
+    // val intent = Intent(this, TargetActivity::class.java)
+    // intent.putExtra("eventId", value) // "key" is a string identifier, value can be any
+    // Serializable or Parcelable object
+    // startActivity(intent)
+    eventID = intent.getStringExtra("eventID") ?: ""
+
+    setContent { AssosTheme { NFCWriting(ticketList = ticketList, eventID = eventID) } }
+
+    // resolveIntent(intent)
+    nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+    if (nfcAdapter == null) {
+      // No NFC Dialog
+      Toast.makeText(this, "NFC is not supported on this device", Toast.LENGTH_SHORT).show()
+      finish() // Finish the activity
+      return
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    if (nfcAdapter?.isEnabled == false) {
+      openNfcSettings()
+    }
+    val pendingIntent =
+        PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+            PendingIntent_Mutable)
+    nfcAdapter?.enableForegroundDispatch(this, pendingIntent, null, null)
+  }
+
+  override fun onPause() {
+    super.onPause()
+    nfcAdapter?.disableForegroundDispatch(this)
+  }
+
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+  public override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+    setIntent(intent)
+    resolveIntent(intent)
+  }
+
+  private fun openNfcSettings() {
+    val intent =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          Intent(Settings.Panel.ACTION_NFC)
+        } else {
+          Intent(Settings.ACTION_WIRELESS_SETTINGS)
         }
+    startActivity(intent)
+  }
 
-        //start l'activity comme ceci (cf. ChatGPT)
-        //val intent = Intent(this, TargetActivity::class.java)
-        //intent.putExtra("eventId", value) // "key" is a string identifier, value can be any Serializable or Parcelable object
-        //startActivity(intent)
-        eventId = intent.getStringExtra("eventId") ?: ""
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+  private fun resolveIntent(intent: Intent) {
+    val validActions =
+        listOf(
+            NfcAdapter.ACTION_TAG_DISCOVERED,
+            NfcAdapter.ACTION_TECH_DISCOVERED,
+            NfcAdapter.ACTION_NDEF_DISCOVERED)
+    if (intent.action in validActions) {
+      val rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
 
-        resolveIntent(intent)
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        if (nfcAdapter == null) {
-            // No NFC Dialog
-            Toast.makeText(this, "NFC is not supported on this device", Toast.LENGTH_SHORT).show()
-            finish() // Finish the activity
-            return
+      if (ticketList.value.isEmpty()) {
+        Toast.makeText(
+                this, "We could not write your tickets, wait a few seconds...", Toast.LENGTH_SHORT)
+            .show()
+      } else {
+        val messageWrittenSuccessfully = createNFCMessage(ticketList.value, intent)
+        if (messageWrittenSuccessfully) {
+          // Message written to tag
+          Toast.makeText(this, "Successfully written to tag", Toast.LENGTH_SHORT).show()
+        } else {
+          // Something went wrong
+          Toast.makeText(this, "Sorry, we could not write your data", Toast.LENGTH_SHORT).show()
         }
+      }
+    } else {
+      // Unknown tag type
+      Toast.makeText(this, "Sorry, we do not know this type of tag", Toast.LENGTH_SHORT).show()
+    }
+  }
+
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+  fun createNFCMessage(payload: List<String>, intent: Intent): Boolean {
+    val pathPrefix = "swent.com:nfcapp"
+    val messageList = mutableListOf<NdefRecord>()
+    payload.forEach {
+      messageList +=
+          NdefRecord(
+              NdefRecord.TNF_EXTERNAL_TYPE,
+              pathPrefix.toByteArray(),
+              ByteArray(0),
+              it.toByteArray())
     }
 
-    override fun onResume() {
-        super.onResume()
-        if (nfcAdapter?.isEnabled == false) {
-            openNfcSettings()
+    val nfcMessage = NdefMessage(messageList.toTypedArray())
+    intent.let {
+      val tag = it.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+      return writeMessageToTag(nfcMessage, tag)
+    }
+  }
+
+  private fun createNFCMessageFromTickets(tickets: List<Ticket>): Boolean {
+    val pathPrefix = "swent.com:nfcapp"
+    val nfcRecord =
+        NdefRecord(
+            NdefRecord.TNF_EXTERNAL_TYPE,
+            pathPrefix.toByteArray(),
+            ByteArray(0),
+            tickets.map { it.id }.toString().toByteArray())
+    val nfcMessage = NdefMessage(arrayOf(nfcRecord))
+    intent.let {
+      val tag = it.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+      return writeMessageToTag(nfcMessage, tag)
+    }
+  }
+
+  private fun writeMessageToTag(nfcMessage: NdefMessage, tag: Tag?): Boolean {
+
+    try {
+      val nDefTag = Ndef.get(tag)
+
+      nDefTag?.let {
+        it.connect()
+        if (it.maxSize < nfcMessage.toByteArray().size) {
+          // Message too large to write to NFC tag
+          return false
         }
-        val pendingIntent =
-            PendingIntent.getActivity(
-                this,
-                0,
-                Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
-                PendingIntent_Mutable)
-        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, null, null)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        nfcAdapter?.disableForegroundDispatch(this)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    public override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        setIntent(intent)
-        resolveIntent(intent)
-    }
-
-    private fun openNfcSettings() {
-        val intent =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                Intent(Settings.Panel.ACTION_NFC)
-            } else {
-                Intent(Settings.ACTION_WIRELESS_SETTINGS)
-            }
-        startActivity(intent)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private fun resolveIntent(intent: Intent) {
-        val validActions =
-            listOf(
-                NfcAdapter.ACTION_TAG_DISCOVERED,
-                NfcAdapter.ACTION_TECH_DISCOVERED,
-                NfcAdapter.ACTION_NDEF_DISCOVERED)
-        if (intent.action in validActions) {
-            val rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
-
-            if (rawMsgs != null) {
-                        val messageWrittenSuccessfully = createNFCMessage("This is a test", intent)
-                        if (messageWrittenSuccessfully) {
-                            // Message written to tag
-                            Toast.makeText(this, "Successfully written to tag", Toast.LENGTH_SHORT).show()
-                        } else {
-                            // Something went wrong
-                            Toast.makeText(this, "Sorry, we could not write your data", Toast.LENGTH_SHORT).show()
-                        }
-                }
-            } else {
-                // Unknown tag type
-                Toast.makeText(this, "Sorry, we do not know this type of tag", Toast.LENGTH_SHORT)
-                    .show()
-
-            }
+        if (it.isWritable) {
+          it.writeNdefMessage(nfcMessage)
+          it.close()
+          // Message is written to tag
+          return true
+        } else {
+          // NFC tag is read-only
+          return false
         }
+      }
 
+      val nDefFormatableTag = NdefFormatable.get(tag)
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    fun createNFCMessage(payload: String, intent: Intent): Boolean {
-        val pathPrefix = "swent.com:nfcapp"
-        val nfcRecord =
-            NdefRecord(
-                NdefRecord.TNF_EXTERNAL_TYPE,
-                pathPrefix.toByteArray(),
-                ByteArray(0),
-                payload.toByteArray())
-        val nfcMessage = NdefMessage(arrayOf(nfcRecord))
-        intent.let {
-            val tag = it.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-            return writeMessageToTag(nfcMessage, tag)
-        }
-    }
-
-    private fun createNFCMessageFromTickets(tickets: List<Ticket>): Boolean {
-        val pathPrefix = "swent.com:nfcapp"
-        val nfcRecord =
-            NdefRecord(
-                NdefRecord.TNF_EXTERNAL_TYPE,
-                pathPrefix.toByteArray(),
-                ByteArray(0),
-                tickets.map { it.id }.toString().toByteArray())
-        val nfcMessage = NdefMessage(arrayOf(nfcRecord))
-        intent.let {
-            val tag = it.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-            return writeMessageToTag(nfcMessage, tag)
-        }
-    }
-
-    private fun writeMessageToTag(nfcMessage: NdefMessage, tag: Tag?): Boolean {
-
+      nDefFormatableTag?.let {
         try {
-            val nDefTag = Ndef.get(tag)
-
-            nDefTag?.let {
-                it.connect()
-                if (it.maxSize < nfcMessage.toByteArray().size) {
-                    // Message too large to write to NFC tag
-                    return false
-                }
-                if (it.isWritable) {
-                    it.writeNdefMessage(nfcMessage)
-                    it.close()
-                    // Message is written to tag
-                    return true
-                } else {
-                    // NFC tag is read-only
-                    return false
-                }
-            }
-
-            val nDefFormatableTag = NdefFormatable.get(tag)
-
-            nDefFormatableTag?.let {
-                try {
-                    it.connect()
-                    it.format(nfcMessage)
-                    it.close()
-                    // The data is written to the tag
-                    return true
-                } catch (e: IOException) {
-                    // Failed to format tag
-                    return false
-                }
-            }
-            // NDEF is not supported
-            return false
-        } catch (e: Exception) {
-            // Write operation has failed
-            throw e
+          it.connect()
+          it.format(nfcMessage)
+          it.close()
+          // The data is written to the tag
+          return true
+        } catch (e: IOException) {
+          // Failed to format tag
+          return false
         }
+      }
+      // NDEF is not supported
+      return false
+    } catch (e: Exception) {
+      // Write operation has failed
     }
+    return false
+  }
 }
