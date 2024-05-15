b0a742fa70cfe0f1aad50a862313ab9e08a0bd62
Réza <reza.machraoui@gmail.com>
2024-05-15 15:13:44 +0200

    add new functions to NFCViewModel

--- app/src/main/java/com/swent/assos/model/view/NFCViewModel.kt
+++ app/src/main/java/com/swent/assos/model/view/NFCViewModel.kt
@@ -1,20 +1,29 @@
 package com.swent.assos.model.view
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.swent.assos.model.di.IoDispatcher
 import com.swent.assos.model.service.DbService
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
-
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class NFCViewModel
 @Inject
 constructor(
-    private val dbService: DbService
+    private val dbService: DbService,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
-}
+  fun checkTicket(ticketId: String, validIDs: List<String>): Boolean {
+    return validIDs.contains(ticketId)
+  }
 
+  fun collectTickets(eventID: String, onSuccess: (List<String>) -> Unit) {
+    viewModelScope.launch(ioDispatcher) {
+      onSuccess(dbService.getTicketsFromEventId(eventID).map { it.id })
+    }
+  }
+}
