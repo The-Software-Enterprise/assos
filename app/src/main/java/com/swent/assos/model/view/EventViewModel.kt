640abb0c54917bc8f473afc6a72fb895a03345ee
alicepotter <alice.potter@epfl.ch>
2024-05-15 13:49:57 +0200

    Added success and failure toasts

--- app/src/main/java/com/swent/assos/model/view/EventViewModel.kt
+++ app/src/main/java/com/swent/assos/model/view/EventViewModel.kt
@@ -51,7 +51,7 @@ constructor(
         }
   }
 
-  fun createEvent(onSuccess: () -> Unit) {
+  fun createEvent(onSuccess: () -> Unit, onError: () -> Unit){
     val event = _event.value
     viewModelScope.launch(ioDispatcher) {
       storageService.uploadFile(
