6fba681c037a6d560991fcc00d7e67bf23d0e926
Jules <jules.herrscher@epfl.ch>
2024-05-16 18:16:08 +0200

    added first instrumented tests

---
+++ app/src/androidTest/java/com/swent/assos/screens/NFCWriterScreen.kt
@@ -0,0 +1,11 @@
+package com.swent.assos.screens
+
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import io.github.kakaocup.compose.node.element.ComposeScreen
+
+class NFCWriterScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<NFCWriterScreen>(
+        semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("NFCWriter") }) {
+
+
+}
