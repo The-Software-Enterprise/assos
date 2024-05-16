332a1ec8d7dfec481c5b6f12beaa808dc60772e1
Jules <jules.herrscher@epfl.ch>
2024-05-16 18:19:08 +0200

    formatting

--- app/src/androidTest/java/com/swent/assos/screens/NFCReaderScreen.kt
+++ app/src/androidTest/java/com/swent/assos/screens/NFCReaderScreen.kt
@@ -5,8 +5,4 @@ import io.github.kakaocup.compose.node.element.ComposeScreen
 
 class NFCReaderScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<NFCReaderScreen>(
-        semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("NFCReader") }) {
-
-
-}
-
+        semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("NFCReader") }) {}
