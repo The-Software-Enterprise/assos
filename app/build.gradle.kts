4f58d7a3a2815761752d9fef119d1cc5a287deb2
Réza <reza.machraoui@gmail.com>
2024-05-21 17:36:01 +0200

    add MLKit to dependencies + setup analyzer for QR Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,8 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
   implementation("com.google.firebase:firebase-functions-ktx:20.4.0")
+  implementation("com.google.mlkit:vision-common:17.3.0")
+  implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
