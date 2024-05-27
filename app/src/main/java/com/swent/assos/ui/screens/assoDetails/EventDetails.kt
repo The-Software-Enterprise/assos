175b503343df4f8148f6a0f806d1a9a9af596cb6
Réza <reza.machraoui@gmail.com>
2024-05-27 23:43:39 +0200

    clarify EventSetup UI

--- app/src/main/java/com/swent/assos/ui/screens/assoDetails/EventDetails.kt
+++ app/src/main/java/com/swent/assos/ui/screens/assoDetails/EventDetails.kt
@@ -1,12 +1,7 @@
 package com.swent.assos.ui.screens.assoDetails
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.FabPosition
@@ -30,7 +25,6 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.swent.assos.R
 import com.swent.assos.model.data.Association
-import com.swent.assos.model.navigation.Destinations
 import com.swent.assos.model.navigation.NavigationActions
 import com.swent.assos.model.view.AssoViewModel
 import com.swent.assos.model.view.EventViewModel
@@ -64,82 +58,57 @@ fun EventDetails(eventId: String, navigationActions: NavigationActions, assoId: 
       modifier = Modifier.semantics { testTagsAsResourceId = true }.testTag("EventDetails"),
       topBar = { PageTitleWithGoBack(title = asso.acronym, navigationActions = navigationActions) },
       floatingActionButton = {
-        if (event.id != "") {
-          when (isMember(myAssociations = myAssociations, currentAsso = assoId)) {
-            true ->
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center) {
-                      if (event.isStaffingEnabled) {
-                        JoinUsButton(
-                            onClick = {
-                              navigationActions.navigateTo(
-                                  Destinations.STAFF_MANAGEMENT.route + "/${eventId}")
-                            },
-                            text = "Staff List")
-                        Spacer(modifier = Modifier.width(10.dp))
-                      }
-                      JoinUsButton(
-                          onClick = {
-                            navigationActions.navigateTo(
-                                Destinations.CREATE_TICKET.route + "/${eventId}")
-                          },
-                          text = "Create ticket")
-                    }
-            false ->
-                if (event.isStaffingEnabled) {
-                  val applied = eventViewModel.appliedStaff.collectAsState()
-                  AssistChip(
-                      colors =
-                          if (applied.value)
-                              AssistChipDefaults.assistChipColors(
-                                  containerColor = MaterialTheme.colorScheme.surfaceVariant)
-                          else
-                              AssistChipDefaults.assistChipColors(
-                                  containerColor = MaterialTheme.colorScheme.secondary),
-                      border = null,
-                      modifier = Modifier.testTag("StaffButton").padding(5.dp),
-                      onClick = {
-                        if (applied.value) {
-                          eventViewModel.removeRequestToStaff(
-                              event.id,
-                              userId.id,
-                              Toast.makeText(
-                                      context,
-                                      "You have successfully removed your request to staff at the event",
-                                      Toast.LENGTH_SHORT)
-                                  .show())
-                        } else {
-                          eventViewModel.applyStaffing(
-                              event.id,
-                              userId.id,
-                              Toast.makeText(
-                                      context,
-                                      "You have successfully applied to staff at the event",
-                                      Toast.LENGTH_SHORT)
-                                  .show())
-                        }
-                      },
-                      label = {
-                        if (applied.value) {
-                          Text(
-                              text = "Remove staff application",
-                              color = MaterialTheme.colorScheme.onSurfaceVariant,
-                              fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-                              fontWeight = FontWeight.Medium,
-                          )
-                        } else {
-                          Text(
-                              text = "Apply for staffing",
-                              color = MaterialTheme.colorScheme.onSecondary,
-                              fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
-                              fontWeight = FontWeight.Medium,
-                          )
-                        }
-                      },
+        if (event.id != "" && !isMember(myAssociations, asso.id) && event.isStaffingEnabled) {
+          val applied = eventViewModel.appliedStaff.collectAsState()
+          AssistChip(
+              colors =
+                  if (applied.value)
+                      AssistChipDefaults.assistChipColors(
+                          containerColor = MaterialTheme.colorScheme.surfaceVariant)
+                  else
+                      AssistChipDefaults.assistChipColors(
+                          containerColor = MaterialTheme.colorScheme.secondary),
+              border = null,
+              modifier = Modifier.testTag("StaffButton").padding(5.dp),
+              onClick = {
+                if (applied.value) {
+                  eventViewModel.removeRequestToStaff(
+                      event.id,
+                      userId.id,
+                      Toast.makeText(
+                              context,
+                              "You have successfully removed your request to staff at the event",
+                              Toast.LENGTH_SHORT)
+                          .show())
+                } else {
+                  eventViewModel.applyStaffing(
+                      event.id,
+                      userId.id,
+                      Toast.makeText(
+                              context,
+                              "You have successfully applied to staff at the event",
+                              Toast.LENGTH_SHORT)
+                          .show())
+                }
+              },
+              label = {
+                if (applied.value) {
+                  Text(
+                      text = "Remove staff application",
+                      color = MaterialTheme.colorScheme.onSurfaceVariant,
+                      fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
+                      fontWeight = FontWeight.Medium,
+                  )
+                } else {
+                  Text(
+                      text = "Apply for staffing",
+                      color = MaterialTheme.colorScheme.onSecondary,
+                      fontFamily = FontFamily(Font(R.font.sf_pro_display_regular)),
+                      fontWeight = FontWeight.Medium,
                   )
                 }
-          }
+              },
+          )
         }
       },
       floatingActionButtonPosition = FabPosition.Center) { paddingValues ->
@@ -147,7 +116,8 @@ fun EventDetails(eventId: String, navigationActions: NavigationActions, assoId: 
             viewModel = eventViewModel,
             paddingValues = paddingValues,
             isMember = isMember(myAssociations = myAssociations, currentAsso = asso.id),
-            eventId = eventId)
+            eventId = eventId,
+            navigationActions = navigationActions)
       }
 }
 
