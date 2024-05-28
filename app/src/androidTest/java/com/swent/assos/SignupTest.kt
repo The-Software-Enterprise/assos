13f03b5e8e2e8aaca29e2a06394d753d565ef170
Jules <jules.herrscher@epfl.ch>
2024-05-28 15:05:01 +0200

    formatting

--- app/src/androidTest/java/com/swent/assos/SignupTest.kt
+++ app/src/androidTest/java/com/swent/assos/SignupTest.kt
@@ -21,11 +21,11 @@ import io.mockk.confirmVerified
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
 import io.mockk.verify
+import java.lang.Thread.sleep
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Thread.sleep
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
