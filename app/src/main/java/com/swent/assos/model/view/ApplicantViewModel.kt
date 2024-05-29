cc1703a52c3f6ef3e2a88a16282bad323316389f
Jules <jules.herrscher@epfl.ch>
2024-05-29 18:31:29 +0200

    formatting

--- app/src/main/java/com/swent/assos/model/view/ApplicantViewModel.kt
+++ app/src/main/java/com/swent/assos/model/view/ApplicantViewModel.kt
@@ -7,11 +7,11 @@ import com.swent.assos.model.data.ParticipationStatus
 import com.swent.assos.model.di.IoDispatcher
 import com.swent.assos.model.service.DbService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @HiltViewModel
 class ApplicantViewModel
