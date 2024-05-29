0b36218b9bcdb8ffbe0e06d45dc148a14b9753e7
Maximilien <maximilien.gridel@gmail.com>
2024-05-29 14:33:47 +0200

    file saved created

--- app/src/main/java/com/swent/assos/ui/screens/profile/Saved.kt
+++ app/src/main/java/com/swent/assos/ui/screens/profile/Saved.kt
@@ -1,2 +1,47 @@
 package com.swent.assos.ui.screens.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.swent.assos.R
+import com.swent.assos.model.navigation.Destinations
+import com.swent.assos.model.navigation.NavigationActions
+import com.swent.assos.model.view.ProfileViewModel
+import com.swent.assos.ui.components.ListItemAsso
+import com.swent.assos.ui.components.LoadingCircle
+import com.swent.assos.ui.components.PageTitleWithGoBack
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun Saved(navigationActions: NavigationActions) {
+
+    Scaffold(
+        modifier = Modifier.semantics { testTagsAsResourceId = true }.testTag("SavedScreen"),
+        topBar = { PageTitleWithGoBack(title = "Saved", navigationActions) },
+    ) { paddingValues ->
+        LazyColumn(
+            contentPadding = paddingValues,
+            modifier =
+            Modifier.testTag("ContentSection")
+                .background(MaterialTheme.colorScheme.background)
+                .padding(16.dp)) {
+
+        }
+    }
+}
