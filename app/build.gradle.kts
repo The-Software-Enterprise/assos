20be94ea6a5a62aaba18927cef18f76be80dc7e4
marinPh <marin.philippe@epfl.ch>
2024-05-30 02:32:56 +0200

    formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,6 @@ dependencies {
   implementation("com.google.firebase:firebase-functions-ktx:20.4.0")
   implementation("com.google.mlkit:vision-common:17.3.0")
   implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
-    implementation("androidx.room:room-common:2.6.1")
     testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
