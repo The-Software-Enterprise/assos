69c9e02a0764d87db51e570f632a5988e681dc7d
marinPh <marin.philippe@epfl.ch>
2024-05-30 11:05:04 +0200

    test corrected

--- app/src/androidTest/java/com/swent/assos/ApplicationsTest.kt
+++ app/src/androidTest/java/com/swent/assos/ApplicationsTest.kt
@@ -3,6 +3,7 @@ package com.swent.assos
 import androidx.activity.compose.setContent
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.firebase.Firebase
@@ -81,11 +82,11 @@ class ApplicationsTest : SuperTest() {
     run {
       ComposeScreen.onComposeScreen<ApplicationsScreen>(composeTestRule) {
         step("Check if the application titles are displayed") {
-          associationsApplicationsTitle { assertIsDisplayed() }
+          composeTestRule.onNodeWithTag("AssociationsApplicationsTitle").assertIsDisplayed()
           staffingApplicationsTitle { assertIsDisplayed() }
           composeTestRule.onNodeWithText("Requested Staffing").assertIsDisplayed()
         }
-        composeTestRule.waitUntil {
+        composeTestRule.waitUntil(10000) {
           composeTestRule.onNodeWithText("PolyLan").isDisplayed() &&
               composeTestRule.onNodeWithText("test event").isDisplayed()
         }
