f1a4675c9ceb67d91a2ade3080e3d9b31f2802eb
Quentin Gross <quentin.gross@epfl.ch>
2024-06-02 22:12:18 +0400

    ktfmtFormat

--- app/src/main/java/com/swent/assos/model/local_database/LocalDatabase.kt
+++ app/src/main/java/com/swent/assos/model/local_database/LocalDatabase.kt
@@ -18,7 +18,8 @@ abstract class LocalDatabase : RoomDatabase() {
 
 @Dao
 interface AssociationDao {
-  @Query("SELECT * FROM associations ORDER BY acronym ASC") fun getAllAssociations(): List<Association>
+  @Query("SELECT * FROM associations ORDER BY acronym ASC")
+  fun getAllAssociations(): List<Association>
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   suspend fun insertAssociation(vararg associations: Association)
