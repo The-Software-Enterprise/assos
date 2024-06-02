1049c9c4e4582e14742238026711c9e54e3c386c
Quentin Gross <quentin.gross@epfl.ch>
2024-06-02 22:38:41 +0400

    Added ticket class to LocalDatabase

--- app/src/main/java/com/swent/assos/model/data/Ticket.kt
+++ app/src/main/java/com/swent/assos/model/data/Ticket.kt
@@ -1,7 +1,11 @@
 package com.swent.assos.model.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tickets")
 data class Ticket(
-    val id: String = "",
+    @PrimaryKey val id: String = "",
     val eventId: String = "",
     val userId: String = "",
     val status: ParticipationStatus = ParticipationStatus.Participant,
